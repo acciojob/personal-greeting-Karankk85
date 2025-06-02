16fc94b8bb7de88e7b61ad64f43f8bce60916418
Karankk85 <karkk8533@gmail.com>
2025-06-02 11:43:31 +0530

    Update App.js
    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [name, setName] = useState("");
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      {name.trim() !== "" && <p>Hello, {name}!</p>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
